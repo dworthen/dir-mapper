d4297921d75e25e6cd53ed1deadb72822e426878
Derek Worthen <derek@derekworthen.com>
2012-09-09 14:46:48 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,146 @@
+# Asynchronous Directory Mapper
+
+Asyncronously recursively maps out a directory structure. Contains tools for organizing and filtering files too.
+
+## Example
+
+This
+
+		root/
+			css/
+				fonts.css
+				style.css
+			images/
+				bg.png
+				me.jpg
+			scripts/
+				jquery.js
+			index.html
+			readme.txt
+
+Will become 
+
+		[ {file: '/path/to/root/css'},
+			{file: 'path/to/root/css/fonts.css'},
+			{file: 'path/to/root/css/style.css'},
+			{file: 'path/to/root/images'},
+			{file: 'path/to/root/images/bg.png'}, 
+			{file: 'path/to/root/images/me.jpg'},
+			{file: 'path/to/root/index.html'},
+			{file: 'path/to/root/scripts'},
+			{file: 'path/to/root/scripts/jquery.js'},
+			{file: 'path/to/root/readme.txt'}
+		]
+
+## Install
+
+		npm install dir-mapper
+
+## API
+
+Directory Mapper supports both a callback based api and an event driven api.
+
+### Callback
+
+		var DirMapper = require('dir-mapper'),
+				root = new DirMapper();
+
+		root.walk('path/to/root', function(error, results) {
+			if (error) throw error;
+			// results.files is an array of objects, as demonstrated above
+		});
+
+### Event
+
+		var root = new (require('dir-mapper'));
+
+		root.on('error', function(err) {
+			// we have an error
+		});
+
+		root.on('end', function(results) {
+			// do stuff
+		});
+
+		root.walk('path/to/root');
+
+### Methods
+
+#### DirMapper([dir ], [recurse], [cb])
+
+- dir: string, directory to walk, if provided.
+- recurse: boolean, to map all sub-directories or not. Defaults to true (Note: this parameter does not make a difference at this time, Directory Mapper will map all sub directories.)
+- cb: a callback function to call once all directories have been walked, otherwise may listen for the end event. 
+		
+		var DirMapper = require('dir-mapper');
+		new DirMapper('path/to/root', function(err, results) {
+			// do stuff
+		});
+
+#### DirMapper.prototype.walk(dir, [recurse], [cb])
+
+Same as the constructor method except dir is required. 
+
+		var DirMapper = require('dir-mapper'),
+			root = new DirMapper();
+
+		root.walk('path/to/root');
+
+#### DirMapper.prototype.sort(order, [field])
+
+Since Directory Mapper maps a directory structure asynchronously there is no garuanteed ordered. This method will organize the resulting list in ascending or descending order.
+
+- order: number, 1 for ascending and -1 for descending.
+- field: the field to order the array of objects by, defaults to 'file'.
+
+		var DirMapper = require('dir-mapper'),
+			root = new DirMapper();
+
+		root.walk('path/to/root', function(err, results) {
+			results.sort(1);
+			// results.files will be in the same order as the example above. 
+		});
+
+#### DirMapper.prototype.flag(field, [fn])
+
+This method, when provided both the field and fn arguments, acts just like the Array.prototype.map method. If only provided the field parameter then this method acts like Array.prototype.filter, returning an array of file objects where each object contains a 'true' value for the field.
+
+- field: string, the field to filter by or add to each file object.
+- fn: function to mark each file object with, receives three arguments fn(el, i, arr)
+	- el: current array element.
+	- i: the current key of the array.
+	- arr: the files array itself.
+
+		var DirMapper = require('dir-mapper');
+		var root = new DirMapper();
+
+		root.walk('path/to/root', function(err, results) {
+			// find which files are in the images directory
+			results.flag('images', function(el) {
+				// true if its in the images directory
+				// it is not necessary to return a boolean, may return a string
+				return /\/images/.test(el.file); 
+			}); 
+			var images = results.flag('images');
+			// sort the new images list in descending order
+			images.sort(-1);
+		});	
+
+## Testing
+
+First run
+
+		npm install --dev
+
+Then
+
+		npm test
+
+## TODO:
+
+- refractor
+- Add the ability to either recurse or not
+- Add synchronous version?
+- Since there is an event driven api maybe convert to a stream??
+- more tests, tests are not comprehensive
+- comment code. 
---
+++ test/dir-mapper.test.js
@@ -0,0 +1,147 @@
+var should = require('should');
+
+describe('async-dir-mapper', function() {
+  
+  var DirMapper = require(__dirname + '/../lib/dir-mapper')
+    , testDir = __dirname + '/fixtures/'
+    , fileObjs;
+    
+  beforeEach(function() {
+    fileObjs = new DirMapper();
+  });
+
+  describe('Constructor1: DirMapper()', function() {
+    it('Should set some configs', function() {
+      fileObjs.should.have.property('files');
+//      fileObjs.should.have.property('filesLeft', 0);
+//      fileObjs.should.have.property('dirsLeft', 1);
+//      fileObjs.should.have.property('recurse', true);
+    });  
+  });
+  
+  describe('Walk:', function() {
+    
+    describe('Walk() - no arguments:', function() {
+      it('should throw an error, DirMapper.walk expects a directory to walk.', function(done) {
+        var test = function(err) {
+          err.should.be.an.instanceOf(Error);
+          err.message.should.be.eql('DirMapper.walk expects a directory to walk.');
+        };
+        
+        // event method
+        fileObjs.on('error', function(err) {
+          test(err);
+          done();
+        });
+
+        // callback method that calls the event based method.
+        fileObjs.walk(undefined, function(err, results) {
+          test(err);
+          fileObjs.walk();
+        });         
+          
+      });
+    });
+    
+    describe('Walk(nonExistentDir):', function() {
+      it('should throw an error, ENOENT, readdir nonExistentDir', function(done) {
+        var test = function(err) {
+          err.should.be.an.instanceOf(Error);
+          err.message.should.be.eql("ENOENT, readdir '/sfg/fdasfd/fdasfd/'");
+        };
+        
+        // Event method
+        fileObjs.on('error', function(err) {
+          test(err);
+          done();
+        });
+        
+        // callback method, also envokes the event method.
+        fileObjs.walk('/sfg/fdasfd/fdasfd/', function(err, results) {
+          test(err);
+          fileObjs.walk('/sfg/fdasfd/fdasfd/');
+        });
+        
+      });
+    });
+    
+    describe('walk(dir, cb):', function() {
+      it('should create an object that contains an array of file objects, calls cb when complete', function(done) {
+        
+        var test = function(results) {
+          results.should.have.property('files');
+          results.files.should.have.length(11);
+        }
+        
+        fileObjs.walk(testDir, function(err, results) {
+          should.not.exist(err);
+          test(results);
+          done();
+        });
+        
+      });
+    });
+
+    describe('walk(dir):', function() {
+      it('should create an object containing an array of file objects, emits event when complete', function(done) {
+        
+        var test = function(results) {
+          results.should.have.property('files');
+          results.files.should.have.length(11);
+        }
+
+        fileObjs.on('error', function(err) {
+          throw err;
+          done();
+        });
+        
+        fileObjs.on('end', function(results) {
+          // should.not.exist(err);
+          // console.log(results);
+          test(results);
+          done();
+        });
+
+        fileObjs.walk(testDir);
+
+      });
+    });
+  });
+
+ describe('sort(order, [field]), 1 for ascending and -1 for descending:', function() {
+   it('it should order the files array by the field according to order', function(done) {
+     fileObjs.walk(testDir, function(err, results) {
+       should.not.exist(err);
+       results.sort(1);
+       /\/images/.test(results.files[0].file).should.be.true;
+       results.sort(1, 'file');
+       /\/images/.test(results.files[0].file).should.be.true;
+       results.sort(-1);
+       /\/style2.css/.test(results.files[0].file).should.be.true;
+       // console.log(fileObjs);
+       done();
+     });
+     
+   });
+ });
+
+  describe('flag(flagName, [fn(el, in, arr)])', function() {
+    it('should check files for specified properties and flag the files', function(done) {
+      fileObjs.walk(testDir, function(err, results) {
+        should.not.exist(err);
+        var newResults = results.flag('file');
+        newResults.sort(1);
+        /\/images/.test(newResults.files[0].file).should.be.true;
+        results.flag('cool', function(el) {
+          return /\/scripts/.test(el.file);
+        });
+        newResults = results.flag('cool');
+        newResults.sort(1);
+        /\/scripts/.test(newResults.files[0].file).should.be.true;
+        done();
+      });
+    });
+  });
+
+  
+});
